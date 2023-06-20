6c60c505299fea27bcdab8d3c3dd28315e5d7b80
RohitRSinha-2705 <rohitsinha2705@gmail.com>
2023-06-21 00:03:26 +0530

    task 3 dom

--- new.js
+++ new.js
@@ -1,2 +1,7 @@
-console.log("Hello World!!!");
-console.log("Hello World!!!");
+var headerTitle = document.getElementById("header-title");
+headerTitle.textContent = "Hello World!!!"
+var header = document.getElementById("main-header");
+header.style.borderBottom = "solid 3px #000";
+var addItem = document.getElementsByClassName("title");
+addItem.style.fontWeight = "bold";
+addItem.style.fontColor = "green";

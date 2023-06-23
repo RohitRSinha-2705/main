47d1c876524065de4b234e597e9e73a1dbb9173f
RohitRSinha-2705 <rohitsinha2705@gmail.com>
2023-06-23 23:46:53 +0530

    DOM task - 04

---
+++ ssss.js
@@ -0,0 +1,5 @@
+var xyz = document.getElementsByClassName('list-group-item');
+xyz[2].style.backgroundColor = 'green';
+for (var i = 0; i< xyz.length; i++){
+    xyz[i].style.fontWeight = 'bold';
+}

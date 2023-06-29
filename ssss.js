b41df6e3d8853e10c5f76b98181f2bc341bcd1bd
RohitRSinha-2705 <rohitsinha2705@gmail.com>
2023-06-29 23:25:38 +0530

    Dom task 9

--- ssss.js
+++ ssss.js
@@ -1,33 +1,21 @@
-// Get input value
-var newItem = document.getElementById('item').value;
-var newDesc = document.getElementById('item-desc').value;
-
-// Create new li element
-var li = document.createElement('li');
-// Add class
-li.className = 'list-group-item';
-// Add text node with input value
-li.appendChild(document.createTextNode(newItem));
-li.appendChild(document.createTextNode(newDesc));
-
-// Create del button element
-var deleteBtn = document.createElement('button');
-var editBtn = document.createElement('button');
-
-
-// Add classes to del button
-deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-editBtn.className = 'btn btn-danger btn-sm float-right edit';
-
-
-// Append text node
-deleteBtn.appendChild(document.createTextNode('X'));
-editBtn.appendChild(document.createTextNode('edit'));
-
-
-// Append button to li
-li.appendChild(deleteBtn);
-li.appendChild(editBtn);
-
-// Append li to list
-itemList.appendChild(li);
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+// Filter event
+filter.addEventListener('keyup', filterItems);
+// Filter Items
+function filterItems(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  // Get lis
+  var items = itemList.getElementsByTagName('li');
+  // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}

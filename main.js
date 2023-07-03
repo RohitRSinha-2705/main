cae5115806a9868f3f77257cd83fad15af512d81
RohitRSinha-2705 <rohitsinha2705@gmail.com>
2023-07-03 23:21:40 +0530

    DoM task 12

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
-var Username = document.getElementById('User-name');
-var Email = document.getElementById('E-mail');
-var form = document.getElementById('booking-form');
-form.addEventListener('submit', addItem);
-function addItem(e){
-    e.preventDefault();
-    localStorage.setItem('Username',Username.value);
-    localStorage.setItem('Email',Email.value);
-    var Username = document.getElementById('User-name').value;
-    var Email = document.getElementById('E-mail').value;
-    localStorage.setItem('Username',Username);
-    localStorage.setItem('Email',Email);
-  }
+e.preventDefault();
+var Username = document.getElementById('User-name').value;
+var Email = document.getElementById('E-mail').value;
+localStorage.setItem('Username',Username);
+localStorage.setItem('Email',Email);
+//localStorage.setItem('Username',Username);
+//localStorage.setItem('Email',Email);
+const obj = {
+  Username,
+  Email
+}
+localStorage.setItem(obj.Username, JSON.stringify(obj));
+showUser(obj);
+
+}
+}
+function showUser(obj){
+const parentItem = document.getElementById("listofitems");
+const childItem = document.createElement("li");
+childItem.textContent = obj.Username + " - " + obj.Email;
+parentItem.append(childItem);
+}

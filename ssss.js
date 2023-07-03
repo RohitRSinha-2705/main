96f513dada9209f9c8657c2c40ca5763a375e51b
RohitRSinha-2705 <rohitsinha2705@gmail.com>
2023-07-03 23:04:32 +0530

    DoM task 11

--- ssss.js
+++ ssss.js
@@ -1,21 +1,14 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
-// Filter event
-filter.addEventListener('keyup', filterItems);
-// Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
-}
+var Username = document.getElementById('User-name');
+var Email = document.getElementById('E-mail');
+var form = document.getElementById('booking-form');
+form.addEventListener('submit', addItem);
+function addItem(e){
+    e.preventDefault();
+    localStorage.setItem('Username',Username.value);
+    localStorage.setItem('Email',Email.value);
+    var Username = document.getElementById('User-name').value;
+    var Email = document.getElementById('E-mail').value;
+    localStorage.setItem('Username',Username);
+    localStorage.setItem('Email',Email);
+
+  }
